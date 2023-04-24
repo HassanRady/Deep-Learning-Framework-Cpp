d90afc205d3f8d51abb8a825691bc918014f59df
HassanRady <hassan.khaled.rady@gmail.com>
2023-04-24 12:05:17 +0200

    forward implementation in linear layer

--- FullyConnected.h
+++ FullyConnected.h
@@ -12,21 +12,42 @@ public: Linear(int inFeatures, int outFeatures) : inputSize(inFeatures), outputS
         trainable = true;
         initializable = true;
 
-        weights = torch::randn({inFeatures, outFeatures}, torch::kCUDA);
-        bias = torch::ones(inFeatures);
+        weights = torch::randn({inFeatures+1, outFeatures}, torch::kCUDA);
+        bias = torch::ones({outFeatures, 1}, torch::kCUDA);
     }
 
-    torch::Tensor forward(const torch::Tensor & inputTensor) {
-        // return torch::multiply(inputTensor, weights);
+    torch::Tensor forward(torch::Tensor & inputTensor) {
+        this->inputTensor = inputTensor;
+        auto inputDims = inputTensor.sizes();
+        batchSize = inputDims[0];
+        inputTensor = torch::cat({inputTensor, torch::ones({batchSize, 1}, torch::kCUDA)}, -1);
         return torch::matmul(inputTensor, weights);
+        // return torch::matmul(inputTensor, weights) + bias;
+}
+
+torch::Tensor backward(torch::Tensor & errorTensor) {
+    gradientWeights = torch::matmul(inputTensor.transpose(1, 0), errorTensor);
+
+    weights = optimizer.update(weights, gradientWeights);
+    
+    return torch::matmul(errorTensor, weights.transpose(1, 0));
 }
 
 private:
     int inputSize;
     int outputSize;
+
     torch::Tensor weights;
+    torch::Tensor gradientWeights;
+
     torch::Tensor bias;
+    torch::Tensor gradientBias;
+
+    torch::Tensor inputTensor;
+
+    int batchSize;
+
 public:
-    Optimizer optimizer;
+    Sgd optimizer;
 };
 
