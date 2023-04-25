4408b4e664cc7d813910235442fbc305a7e17c06
HassanRady <hassan.khaled.rady@gmail.com>
2023-04-25 10:55:43 +0200

    sgd with momentum

--- Optimizer.h
+++ Optimizer.h
@@ -2,25 +2,41 @@
 // Created by hassan on 22.04.23.
 //
 
-
 #include <torch/torch.h>
 
-
 class Optimizer {
-public: Optimizer() {}
-public: Optimizer(double learningRate) : learningRate(learningRate){}
+public:
+    Optimizer() {}
+    Optimizer(double learningRate) : learningRate(learningRate) {}
+
+    virtual torch::Tensor update(torch::Tensor &weightTensor, const torch::Tensor &gradientTensor) = 0;
 
 protected:
     double learningRate;
 };
 
-class Sgd: public Optimizer{
+class Sgd : public Optimizer {
+public:
+    Sgd(double learningRate) : Optimizer(learningRate) {}
 
-public: Sgd(double learningRate) : Optimizer(learningRate) {}
+    Sgd() {}
 
-torch::Tensor update(torch::Tensor & weightTensor, const torch::Tensor & gradientTensor) {
-    weightTensor = weightTensor - learningRate * gradientTensor;
-    return weightTensor;
-}
-
+    torch::Tensor update(torch::Tensor &weightTensor, const torch::Tensor &gradientTensor) override {
+        weightTensor = weightTensor - learningRate * gradientTensor;
+        return weightTensor;
+    }
 };
+
+
+class SgdWithMomentum : public Optimizer {
+public:
+    SgdWithMomentum(double learningRate, double momentum) : Optimizer(learningRate), momentum(momentum) {}
+
+    torch::Tensor update(torch::Tensor &weightTensor, torch::Tensor &gradientTensor) override {
+        auto v = momentum * v - learningRate * gradientTensor;
+        return weightTensor = weightTensor + v;
+    }
+
+private:
+    double momentum;
+}
