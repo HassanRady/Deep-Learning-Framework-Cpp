5dce10c250f13530f88529ae60328bd3c955e50d
HassanRady <hassan.khaled.rady@gmail.com>
2023-05-11 14:33:55 +0200

    Dropout implementation done

--- Dropout.h
+++ Dropout.h
@@ -2,24 +2,26 @@
 #include <torch/torch.h>
 #include "Base.h"
 
-class Dropout: public BaseLayer {
+class Dropout : public BaseLayer {
 public:
-    Dropout(float probability=0.5): probability(probability) {
+    Dropout(float probability = 0.5) : probability(probability) {
         trainable = false;
         initializable = false;
     }
 
-    torch::Tensor forward(troch::Tensor & inputTensor) {
+    torch::Tensor forward(torch::Tensor &inputTensor) {
         if (testingPhase)
             return inputTensor;
 
-//        mask = torch::rand({inputTensor.index()})
-        return inputTensor;
+        auto tensorShape = inputTensor.sizes();
+        mask = torch::rand({tensorShape[tensorShape.size() - 2], tensorShape[tensorShape.size() - 1]}).to(inputTensor.device());
+        return mask * inputTensor;
     }
 
-    torch::Tensor backward(torch::Tensor & errorTensor) {
-
-        return errorTensor;
+    torch::Tensor backward(torch::Tensor &errorTensor) {
+        auto out = errorTensor * mask;
+        out = out / probability;
+        return out;
     }
 
 private:
