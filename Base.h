4b2594afc74b3661bd977668dbd82b6a3120ec53
HassanRady <hassan.khaled.rady@gmail.com>
2023-06-03 11:40:43 +0200

    adding eval & train for phases

--- Base.h
+++ Base.h
@@ -1,11 +1,22 @@
 #pragma once
 
 class BaseLayer {
-protected: BaseLayer() {
+protected:
+    BaseLayer() {
         trainable = false;
         initializable = false;
+        training = true;
+    }
+
+    int train() {
+        training = true;
+    }
+
+    int eval() {
+        training = false;
     }
 
     bool trainable;
     bool initializable;
+    bool training;
 };
