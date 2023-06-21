cd0cdaf847da7da000888f43d62a1681a7a0c2ec
HassanRady <hassan.khaled.rady@gmail.com>
2023-06-21 16:08:38 +0200

    formatting

--- Loss.h
+++ Loss.h
@@ -4,24 +4,26 @@
 #include <torch/torch.h>
 #include "Base.h"
 
-class CrossEntropyLoss: public BaseLayer {
+class CrossEntropyLoss : public BaseLayer
+{
 public:
-    CrossEntropyLoss(float epsilon=1e-09) {
+    CrossEntropyLoss(float epsilon = 1e-09)
+    {
         this->epsilon = epsilon;
         trainable = false;
         initializable = false;
     }
 
-    float forward(torch::Tensor & y_hat, torch::Tensor & y)  override{
+    float forward(torch::Tensor &y_hat, torch::Tensor &y) override
+    {
         this->y = y;
         auto product = y * torch::log(y_hat + epsilon);
-        float output = -product.sum().item<float>();
-        std::cout << output << "\n";
-        return output;
+        return -product.sum().item<float>();
     }
 
-    torch::Tensor backward(torch::Tensor & grad_y) override{
-        return torch::where(grad_y.eq(1), -grad_y/(y + epsilon), 0);
+    torch::Tensor backward(torch::Tensor &grad_y) override
+    {
+        return torch::where(grad_y.eq(1), -grad_y / (y + epsilon), 0);
     }
 
 private:
