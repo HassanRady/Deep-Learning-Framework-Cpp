d60bb33dcc7572231e6052ee573e10c0e5016198
HassanRady <hassan.khaled.rady@gmail.com>
2023-07-02 19:28:01 +0200

    modifying reference

--- Optimizer.h
+++ Optimizer.h
@@ -1,40 +1,61 @@
 #pragma once
 
+#include "iostream"
 #include <torch/torch.h>
 
-class Optimizer {
+class Optimizer
+{
 public:
     Optimizer() {}
 
     Optimizer(double learningRate) : learningRate(learningRate) {}
 
-    virtual torch::Tensor &update(torch::Tensor &weightTensor, const torch::Tensor &gradientTensor) = 0;
+    virtual int update(torch::Tensor &weightTensor, const torch::Tensor &gradientTensor) = 0;
+    virtual ~Optimizer() = 0;
 
-protected:
+public:
     double learningRate;
 };
 
-class Sgd : public Optimizer {
+Optimizer::~Optimizer() {
+    std::cout << "Pure virtual destructor is called \n";
+}
+
+class Sgd : public Optimizer
+{
 public:
     Sgd(double learningRate) : Optimizer(learningRate) {}
 
     Sgd() {}
 
-    torch::Tensor &update(torch::Tensor &weightTensor, const torch::Tensor &gradientTensor) override {
+    int update(torch::Tensor &weightTensor, const torch::Tensor &gradientTensor) override
+    {
         weightTensor = weightTensor - learningRate * gradientTensor;
-        return weightTensor;
+        return 0;
+    }
+
+    ~Sgd()
+    {
+        std::cout << "sgd deleted"
+                  << "\n";
     }
 };
 
-
-class SgdWithMomentum : public Optimizer {
+class SgdWithMomentum : public Optimizer
+{
 public:
     SgdWithMomentum(double learningRate, double momentum) : Optimizer(learningRate), momentum(momentum) {}
 
-    torch::Tensor &update(torch::Tensor &weightTensor, const torch::Tensor &gradientTensor) override {
+    int update(torch::Tensor &weightTensor, const torch::Tensor &gradientTensor) override
+    {
         v = momentum * v - learningRate * gradientTensor;
         weightTensor = weightTensor + v;
-        return weightTensor;
+        return 0;
+    }
+    ~SgdWithMomentum()
+    {
+        std::cout << "SgdWithMomentum deleted"
+                  << "\n";
     }
 
 private:
@@ -42,13 +63,16 @@ private:
     torch::Tensor v = torch::zeros({1}, torch::kCUDA);
 };
 
-class Adam : public Optimizer {
+class Adam : public Optimizer
+{
 public:
     Adam(double learningRate = 0.001, double mu = 0.9, double rho = 0.9, double epsilon = 1e-07) : Optimizer(
-            learningRate), mu(mu), rho(rho),
+                                                                                                       learningRate),
+                                                                                                   mu(mu), rho(rho),
                                                                                                    epsilon(epsilon) {}
 
-    torch::Tensor &update(torch::Tensor &weightTensor, const torch::Tensor &gradientTensor) override {
+    int update(torch::Tensor &weightTensor, const torch::Tensor &gradientTensor) override
+    {
         k = k + 1;
 
         v = mu * v + (1 - mu) * gradientTensor;
@@ -61,12 +85,17 @@ public:
         auto rHat = (r) / (1 - pow(rho, k));
 
         weightTensor = weightTensor - learningRate * (vHat) / (pow(rHat, 2) + epsilon);
-        return weightTensor;
+        return 0;
+    }
+
+    ~Adam()
+    {
+        std::cout << "Adam deleted"
+                  << "\n";
     }
 
 private:
     double mu, rho, epsilon, k = 0.0;
     torch::Tensor v = torch::zeros({1}, torch::kCUDA);
     torch::Tensor r = torch::zeros({1}, torch::kCUDA);
-
 };
