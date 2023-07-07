0edf13094d707d5bc4924878b4851222f3c64a5a
HassanRady <hassan.khaled.rady@gmail.com>
2023-07-07 16:20:47 +0200

    remove default destructor definition

--- src/Initializers/He.cpp
+++ src/Initializers/He.cpp
@@ -9,5 +9,3 @@ void He::initialize(torch::Tensor &tensor, int fanIn, int fanOut)
     auto sigma = std::sqrt((float)(2) / (fanIn + fanOut));
     torch::nn::init::normal_(tensor, 0, sigma);
 }
-
-He::~He() {}
