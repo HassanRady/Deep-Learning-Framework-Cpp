49e479deefcd7212cbaa2d795575f2fb9ba3b791
HassanRady <hassan.khaled.rady@gmail.com>
2023-07-08 18:38:41 +0200

    using namespace

--- src/Layers/BatchNormalization.cpp
+++ src/Layers/BatchNormalization.cpp
@@ -7,71 +7,71 @@ BatchNorm2d::BatchNorm2d(int numFeatures, float eps = 1e-11, float momentum = 0.
     trainable = true;
     initializable = false;
 
-    this->numFeatures = numFeatures;
-    this->eps = eps;
-    this->momentum = momentum;
+    BatchNorm2d::numFeatures = numFeatures;
+    BatchNorm2d::eps = eps;
+    BatchNorm2d::momentum = momentum;
 
-    weight = torch::ones({numFeatures}, torch::kCUDA);
-    bias = torch::zeros({numFeatures}, torch::kCUDA);
+    BatchNorm2d::weight = torch::ones({numFeatures}, torch::kCUDA);
+    BatchNorm2d::bias = torch::zeros({numFeatures}, torch::kCUDA);
 
-    mean = torch::zeros({numFeatures}, torch::kCUDA);
-    variance = torch::ones({numFeatures}, torch::kCUDA);
+    BatchNorm2d::mean = torch::zeros({numFeatures}, torch::kCUDA);
+    BatchNorm2d::variance = torch::ones({numFeatures}, torch::kCUDA);
 }
 
 torch::Tensor BatchNorm2d::normalizeTrain(torch::Tensor &tensor)
 {
     torch::Tensor batchMean = tensor.mean({0, 2, 3}, false);
     torch::Tensor batchVariance = tensor.var({0, 2, 3}, false);
-    torch::Tensor batchStd = torch::sqrt(batchVariance + eps);
+    torch::Tensor batchStd = torch::sqrt(batchVariance + BatchNorm2d::eps);
     auto n = tensor.numel() / tensor.sizes()[1];
 
-    mean = momentum * batchMean + (1 - momentum) * mean;
-    variance = momentum * batchVariance * n / (n - 1) + (1 - momentum) * variance;
+    mean = BatchNorm2d::momentum * batchMean + (1 - BatchNorm2d::momentum) * BatchNorm2d::mean;
+    variance = BatchNorm2d::momentum * batchVariance * n / (n - 1) + (1 - BatchNorm2d::momentum) * BatchNorm2d::variance;
 
-    inputTensorNormalized = (tensor - batchMean.unsqueeze(0).unsqueeze(2).unsqueeze(3)) /
+    BatchNorm2d::inputTensorNormalized = (tensor - batchMean.unsqueeze(0).unsqueeze(2).unsqueeze(3)) /
                             batchStd.unsqueeze(0).unsqueeze(2).unsqueeze(3);
-    torch::Tensor inputBatchNormalized = weight.unsqueeze(0).unsqueeze(2).unsqueeze(3) * inputTensorNormalized +
-                                         bias.unsqueeze(0).unsqueeze(2).unsqueeze(3);
+    torch::Tensor inputBatchNormalized = BatchNorm2d::weight.unsqueeze(0).unsqueeze(2).unsqueeze(3) * BatchNorm2d::inputTensorNormalized +
+                                         BatchNorm2d::bias.unsqueeze(0).unsqueeze(2).unsqueeze(3);
     return inputBatchNormalized;
 }
 
 torch::Tensor BatchNorm2d::normalizeTest(torch::Tensor &tensor)
 {
-    torch::Tensor inputTensorNormalize = (tensor - mean.unsqueeze(0).unsqueeze(2).unsqueeze(3)) /
+    torch::Tensor inputTensorNormalize = (tensor - BatchNorm2d::mean.unsqueeze(0).unsqueeze(2).unsqueeze(3)) /
                                          torch::sqrt(variance.unsqueeze(0).unsqueeze(2).unsqueeze(3) + eps);
-    torch::Tensor inputNormalized = weight.unsqueeze(0).unsqueeze(2).unsqueeze(3) * inputTensorNormalize +
-                                    bias.unsqueeze(0).unsqueeze(2).unsqueeze(3);
+    torch::Tensor inputNormalized = BatchNorm2d::weight.unsqueeze(0).unsqueeze(2).unsqueeze(3) * inputTensorNormalize +
+                                    BatchNorm2d::bias.unsqueeze(0).unsqueeze(2).unsqueeze(3);
     return inputNormalized;
 }
 
 torch::Tensor BatchNorm2d::forward(torch::Tensor &inputTensor) 
 {
-    this->inputTensor = inputTensor;
-    batchSize = inputTensor.sizes()[0];
+    BatchNorm2d::inputTensor = inputTensor;
+    BatchNorm2d::batchSize = inputTensor.sizes()[0];
 
     torch::Tensor inputNormalized;
 
-    if (training)
-        inputNormalized = normalizeTrain(inputTensor);
+    if (BatchNorm2d::training)
+        inputNormalized = BatchNorm2d::normalizeTrain(inputTensor);
     else
-        inputNormalized = normalizeTest(inputTensor);
+        inputNormalized = BatchNorm2d::normalizeTest(inputTensor);
 
     return inputNormalized;
 }
 
 torch::Tensor BatchNorm2d::backward(torch::Tensor &errorTensor) 
 {
-    torch::Tensor gradientWeight = torch::sum(errorTensor * inputTensorNormalized, {0, 2, 3});
+    torch::Tensor gradientWeight = torch::sum(errorTensor * BatchNorm2d::inputTensorNormalized, {0, 2, 3});
     torch::Tensor gradientBias = errorTensor.sum({0, 2, 3});
-    torch::Tensor gradientInputNormalized = errorTensor * weight.unsqueeze(0).unsqueeze(2).unsqueeze(3);
+    torch::Tensor gradientInputNormalized = errorTensor * BatchNorm2d::weight.unsqueeze(0).unsqueeze(2).unsqueeze(3);
 
     torch::Tensor gradientInputTensor =
-        1 / (batchSize * torch::sqrt(variance.unsqueeze(0).unsqueeze(2).unsqueeze(3) + eps) *
-                 (batchSize * gradientInputNormalized - gradientInputNormalized.sum({0})) -
-             inputTensorNormalized * torch::sum(gradientInputNormalized * inputTensorNormalized, {0}));
+        1 / (BatchNorm2d::batchSize * torch::sqrt(BatchNorm2d::variance.unsqueeze(0).unsqueeze(2).unsqueeze(3) + BatchNorm2d::eps) *
+                 (BatchNorm2d::batchSize * gradientInputNormalized - gradientInputNormalized.sum({0})) -
+             BatchNorm2d::inputTensorNormalized * torch::sum(gradientInputNormalized * BatchNorm2d::inputTensorNormalized, {0}));
 
-    optimizer->update(weight, gradientWeight);
-    optimizer->update(bias, gradientBias);
+    BatchNorm2d::optimizer->update(BatchNorm2d::weight, gradientWeight);
+    BatchNorm2d::optimizer->update(BatchNorm2d::bias, gradientBias);
 
     return gradientInputTensor;
 }
--- src/Losses/CrossEntropy.cpp
+++ src/Losses/CrossEntropy.cpp
@@ -4,17 +4,17 @@ using namespace DeepStorm::Losses;
 
 CrossEntropyLoss::CrossEntropyLoss(float epsilon = 1e-09)
 {
-    this->epsilon = epsilon;
+    CrossEntropyLoss::epsilon = epsilon;
 }
 
 float CrossEntropyLoss::forward(torch::Tensor &y_hat, torch::Tensor &y) 
 {
-    this->y = y;
-    auto product = y * torch::log(y_hat + epsilon);
+    CrossEntropyLoss::y = y;
+    auto product = y * torch::log(y_hat + CrossEntropyLoss::epsilon);
     return -product.sum().item<float>();
 }
 
 torch::Tensor CrossEntropyLoss::backward(torch::Tensor &grad_y) 
 {
-    return torch::where(grad_y.eq(1), -grad_y / (y + epsilon), 0);
+    return torch::where(grad_y.eq(1), -grad_y / (CrossEntropyLoss::y + CrossEntropyLoss::epsilon), 0);
 }
