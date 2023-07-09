278c08f88d06a716c57948156be0ee0e8d084bc6
HassanRady <hassan.khaled.rady@gmail.com>
2023-07-09 18:08:36 +0200

    allocating on heap

--- example.cpp
+++ example.cpp
@@ -50,46 +50,25 @@ int main()
 
     DeepStorm::Initializers::Constant bInit(1);
 
-    Conv2d conv1(inChannels, outChannels, filterSize, stride, padding, &wInit, &bInit);
-    Conv2d conv2(outChannels, outChannels, filterSize, stride, padding, &wInit, &bInit);
-    BatchNorm2d batchNorm1(16, 1e-11, 0.8);
-    BatchNorm2d batchNorm2(16, 1e-11, 0.8);
-    Dropout dropout(0.3);
-    MaxPool2d maxPool1(2, 2);
-    MaxPool2d maxPool2(2, 2);
-    ReLU relu1;
-    ReLU relu2;
-    ReLU relu3;
-    Flatten flatten;
-    Linear fc1(outChannels * 7 * 7, 32, &wInit, &bInit);
-    Linear fc2(32, classes, &wInit, &bInit);
-    SoftMax softmax;
-
     Adam optimizer();
     CrossEntropyLoss loss(1e-09);
 
-    auto model = Model(vector<Layer *>{
-        &conv1,
-        &batchNorm1,
-        &dropout,
-        &relu1,
-        &maxPool1,
-        &conv2,
-        &batchNorm2,
-        &relu2,
-        &maxPool2,
-        &flatten,
-        &fc1,
-        &relu3,
-        &fc2,
-        &softmax});
+    Model model = Model();
 
-    conv1.optimizer = new Adam(0.001, 0.9, 0.9, 1e-07);
-    conv2.optimizer = new Adam(0.001, 0.9, 0.9, 1e-07);
-    batchNorm1.optimizer = new Adam(0.001, 0.9, 0.9, 1e-07);
-    batchNorm2.optimizer = new Adam(0.001, 0.9, 0.9, 1e-07);
-    fc1.optimizer = new Adam(0.001, 0.9, 0.9, 1e-07);
-    fc2.optimizer = new Adam(0.001, 0.9, 0.9, 1e-07);
+    model.append(new Conv2d(inChannels, outChannels, filterSize, stride, padding, &wInit, &bInit));
+    model.append(new BatchNorm2d(16, 1e-11, 0.8));
+    model.append(new Dropout(0.3));
+    model.append(new ReLU());
+    model.append(new MaxPool2d(2, 2));
+    model.append(new Conv2d(outChannels, outChannels, filterSize, stride, padding, &wInit, &bInit));
+    model.append(new BatchNorm2d(16, 1e-11, 0.8));
+    model.append(new ReLU());
+    model.append(new MaxPool2d(2, 2));
+    model.append(new Flatten());
+    model.append(new Linear(outChannels*7*7, 32, &wInit, &bInit));
+    model.append(new ReLU());
+    model.append(new Linear(32, classes, &wInit, &bInit));
+    model.append(new SoftMax());
 
     auto trainset = ImgDataset("./data/trainset", 1, (unsigned)1);
     auto valset = ImgDataset("./data/trainset", 1, (unsigned)2);
@@ -106,5 +85,5 @@ int main()
 
     auto trainer = Trainer(model, &loss, batchSize);
 
-    auto [x, y] = trainer.fit<>(*trainLoader, *valLoader, 5);
+    auto [x, y] = trainer.fit<>(*trainLoader, *valLoader, 3);
 }
