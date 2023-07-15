ce42074fc72654a62ac078d430ba2c01293b8a8e
HassanRady <hassan.khaled.rady@gmail.com>
2023-07-15 12:29:07 +0200

    fix casting

--- src/Initializers/He.cpp
+++ src/Initializers/He.cpp
@@ -6,6 +6,6 @@ He::He() {}
 
 void He::initialize(torch::Tensor &tensor, int fanIn, int fanOut)
 {
-    auto sigma = std::sqrt((float)(2) / (fanIn + fanOut));
+    auto sigma = std::sqrt((2.0) / (fanIn));
     torch::nn::init::normal_(tensor, 0, sigma);
 }
