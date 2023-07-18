364cb8f2137c655997e659b57ef7c4a13b5da812
HassanRady <hassan.khaled.rady@gmail.com>
2023-07-18 12:40:44 +0200

    updating example

--- example.cpp
+++ example.cpp
@@ -12,15 +12,16 @@
 #include "Linear.hpp"
 #include "Pooling.hpp"
 #include "Relu.hpp"
+#include "Sigmoid.hpp"
 #include "Softmax.hpp"
 #include "He.hpp"
 #include "Constant.hpp"
 #include "Optimizer.hpp"
 #include "Adam.hpp"
+#include "SGD.hpp"
 #include "ImgDataset.hpp"
 #include "Trainer.hpp"
 #include "CrossEntropy.hpp"
-
 
 using namespace std;
 
@@ -34,9 +35,9 @@ using namespace DeepStorm::Losses;
 
 int main()
 {
-    torch::manual_seed(1);
+    // torch::manual_seed(1);
 
-    auto batchSize = 4;
+    auto batchSize = 32;
     auto inChannels = 1;
     auto filterSize = 3;
     auto outChannels = 16;
@@ -46,27 +47,27 @@ int main()
 
     CrossEntropyLoss loss(1e-09);
 
-    Model model = Model();
+    Model *model = new Model();
 
-    model.append(new Conv2d(inChannels, outChannels, filterSize, stride, padding, new He(), new Constant(1), new Adam(0.001, 0.9, 0.9, 1e-07)));
-    model.append(new BatchNorm2d(16, new Adam(0.001, 0.9, 0.9, 1e-07), 1e-11, 0.8));
-    model.append(new Dropout(0.3));
-    model.append(new ReLU());
-    model.append(new MaxPool2d(2, 2));
-    model.append(new Conv2d(outChannels, outChannels, filterSize, stride, padding, new He(), new Constant(1), new Adam(0.001, 0.9, 0.9, 1e-07)));
-    model.append(new BatchNorm2d(16, new Adam(0.001, 0.9, 0.9, 1e-07), 1e-11, 0.8));
-    model.append(new ReLU());
-    model.append(new MaxPool2d(2, 2));
-    model.append(new Flatten());
-    model.append(new Linear(outChannels*7*7, 32, new He(), new Constant(1), new Adam(0.001, 0.9, 0.9, 1e-07)));
-    model.append(new ReLU());
-    model.append(new Linear(32, classes, new He(), new Constant(1), new Adam(0.001, 0.9, 0.9, 1e-07)));
-    model.append(new SoftMax());
+    model->append(new Conv2d(inChannels, outChannels, filterSize, stride, padding, new He(), new Constant(0.01), new Sgd(1e-3)));
+    model->append(new BatchNorm2d(outChannels, new Adam(0.001, 0.9, 0.9, 1e-07), 1e-11, 0.8));
+    model->append(new Dropout(0.3));
+    model->append(new ReLU());
+    model->append(new MaxPool2d(2, 2));
+    model->append(new Conv2d(outChannels, outChannels, filterSize, stride, padding, new He(), new Constant(0.01), new Adam(0.001, 0.9, 0.9, 1e-07)));
+    model->append(new BatchNorm2d(outChannels, new Adam(0.001, 0.9, 0.9, 1e-07), 1e-11, 0.8));
+    model->append(new ReLU());
+    model->append(new MaxPool2d(2, 2));
+    model->append(new Flatten());
+    model->append(new Linear(outChannels * 7 * 7, 32, new He(), new Constant(0.01), new Sgd(1e-3)));
+    model->append(new ReLU());
+    model->append(new Linear(32, classes, new He(), new Constant(0.01), new Adam(0.001, 0.9, 0.9, 1e-07)));
+    model->append(new SoftMax());
 
-    auto trainset = ImgDataset("./data/trainset", 1, (unsigned)1);
+    auto trainset = ImgDataset("./data/trainset", 1, (unsigned)9);
     auto valset = ImgDataset("./data/trainset", 1, (unsigned)2);
 
-    trainset.resize(8);
+    trainset.resize(256);
     valset.resize(2);
 
     auto trainLoader = torch::data::make_data_loader(std::move(trainset.map(torch::data::transforms::Stack<>())),
@@ -75,8 +76,9 @@ int main()
     auto valLoader = torch::data::make_data_loader(std::move(valset.map(torch::data::transforms::Stack<>())),
                                                    torch::data::DataLoaderOptions().batch_size(batchSize).drop_last(true));
 
-
     auto trainer = Trainer(model, &loss, batchSize);
 
-    auto [x, y] = trainer.fit<>(*trainLoader, *valLoader, 3);
+    auto [x, y] = trainer.fit<>(*trainLoader, *valLoader, 20);
+
+    delete model;
 }
