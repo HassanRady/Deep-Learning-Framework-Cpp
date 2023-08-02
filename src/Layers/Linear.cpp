ffca005aa0fb10f608787e0ee03092cd8ea38c60
HassanRady <hassan.khaled.rady@gmail.com>
2023-08-02 12:55:54 +0200

    cleaning

--- src/Layers/Linear.cpp
+++ src/Layers/Linear.cpp
@@ -27,7 +27,7 @@ void Linear::initialize()
     Linear::weightInitializer->initialize(Linear::weights, Linear::inFeatures, Linear::outFeatures);
     Linear::biasInitializer->initialize(Linear::bias, 1, Linear::outFeatures);
 
-    // Linear::weights = torch::cat({Linear::weights, Linear::bias}, 0);
+    Linear::weights = torch::cat({Linear::weights, Linear::bias}, 0);
 }
 
 torch::Tensor Linear::forward(torch::Tensor &inputTensor) 
@@ -35,18 +35,14 @@ torch::Tensor Linear::forward(torch::Tensor &inputTensor)
     auto inputDims = inputTensor.sizes();
     Linear::batchSize = inputDims[0];
 
-    // Linear::inputTensor = torch::cat({inputTensor, torch::ones({batchSize, 1}, torch::kCUDA)}, -1);
-    Linear::inputTensor = inputTensor;
-    auto out = torch::matmul(inputTensor, Linear::weights) + Linear::bias; 
-    return out;
+    Linear::inputTensor = torch::cat({inputTensor, torch::ones({batchSize, 1}, torch::kCUDA)}, -1);
+
+    return torch::matmul(Linear::inputTensor, Linear::weights); 
 }
 
 torch::Tensor Linear::backward(torch::Tensor &errorTensor) 
 {
     Linear::gradientWeights = torch::matmul(Linear::inputTensor.transpose(1, 0), errorTensor);
-    // Linear::gradientBias =  errorTensor.sum(-1, false);
     Linear::weights = optimizer->update(Linear::weights, Linear::gradientWeights);
-    // optimizer->update(Linear::bias, Linear::gradientBias);
-    // return torch::matmul(errorTensor, Linear::weights.transpose(1, 0)).index({torch::indexing::Slice(), torch::indexing::Slice(0, Linear::inFeatures)});
-    return torch::matmul(errorTensor, Linear::weights.transpose(1, 0));
+    return torch::matmul(errorTensor, Linear::weights.transpose(1, 0)).index({torch::indexing::Slice(), torch::indexing::Slice(0, Linear::inFeatures)});
 }
