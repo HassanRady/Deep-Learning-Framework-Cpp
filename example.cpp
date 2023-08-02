9a776910036886de47f57392f401eb0c6608fb8e
HassanRady <hassan.khaled.rady@gmail.com>
2023-08-02 16:50:18 +0200

    updating example

--- example.cpp
+++ example.cpp
@@ -35,9 +35,8 @@ using namespace DeepStorm::Losses;
 
 int main()
 {
-    // torch::manual_seed(1);
+    torch::manual_seed(1);
 
-    auto batchSize = 6;
     auto inChannels = 1;
     auto filterSize = 3;
     auto outChannels = 16;
@@ -45,41 +44,49 @@ int main()
     auto padding = "same";
     auto classes = 10;
 
-    CrossEntropyLoss loss(1e-09);
+    std::shared_ptr<Loss> loss = std::make_shared<CrossEntropyLoss>(1e-09);
+    std::shared_ptr<Model> model = std::make_shared<Model>();
 
-    // Model *model = new Model();
-    std::unique_ptr<Model> model = std::make_unique<Model>();
-
-    model->append(new Conv2d(inChannels, outChannels, filterSize, stride, padding, new He(), new Constant(0.01), new Sgd(1e-3)));
+    // model->append(new Conv2d(inChannels, outChannels, filterSize, stride, padding, new He(), new Constant(0.01), new Adam(0.001, 0.9, 0.9, 1e-07)));
     // model->append(new BatchNorm2d(outChannels, new Adam(0.001, 0.9, 0.9, 1e-07), 1e-11, 0.8));
     // model->append(new Dropout(0.3));
-    model->append(new ReLU());
-    // model->append(new MaxPool2d(2, 2));
-    model->append(new Conv2d(outChannels, outChannels, filterSize, stride, padding, new He(), new Constant(0.01), new Adam(0.001, 0.9, 0.9, 1e-07)));
-    // model->append(new BatchNorm2d(outChannels, new Adam(0.001, 0.9, 0.9, 1e-07), 1e-11, 0.8));
-    model->append(new ReLU());
-    // model->append(new MaxPool2d(2, 2));
-    model->append(new Flatten());
-    model->append(new Linear(outChannels * 28 * 28, 32, new He(), new Constant(0.01), new Sgd(1e-3)));
     // model->append(new ReLU());
-    model->append(new Linear(32, classes, new He(), new Constant(0.01), new Adam(0.001, 0.9, 0.9, 1e-07)));
+    // model->append(new MaxPool2d(2, 2));
+    // model->append(new Conv2d(outChannels, outChannels, filterSize, stride, padding, new He(), new Constant(0.01), new Adam(0.001, 0.9, 0.9, 1e-07)));
+    // model->append(new BatchNorm2d(outChannels, new Adam(0.001, 0.9, 0.9, 1e-07), 1e-11, 0.8));
+    // model->append(new ReLU());
+    // model->append(new MaxPool2d(2, 2));
+    // model->append(new Flatten());
+    // model->append(new Linear(outChannels * 7 * 7, 128, new He(), new Constant(0.01), new Adam(0.001, 0.9, 0.9, 1e-07)));
+    // model->append(new ReLU());
+    // model->append(new Linear(128, classes, new He(), new Constant(0.01), new Adam(0.001, 0.9, 0.9, 1e-07)));
+    // model->append(new ReLU());
+    // model->append(new SoftMax());
+
+    model->append(new Linear(28 * 28, 256, new He(), new Constant(0.01), new Adam(0.001, 0.9, 0.9, 1e-07)));
+    model->append(new ReLU());
+    model->append(new Linear(256, 128, new He(), new Constant(0.01), new Adam(0.001, 0.9, 0.9, 1e-07)));
+    model->append(new ReLU());
+    model->append(new Linear(128, 64, new He(), new Constant(0.01), new Adam(0.001, 0.9, 0.9, 1e-07)));
+    model->append(new ReLU());
+    model->append(new Linear(64, 10, new He(), new Constant(0.01), new Adam(0.001, 0.9, 0.9, 1e-07)));
     model->append(new ReLU());
     model->append(new SoftMax());
 
     auto trainset = ImgDataset("./data/trainset", 1, (unsigned)9);
     auto valset = ImgDataset("./data/trainset", 1, (unsigned)2);
 
-    trainset.resize(12);
-    valset.resize(2);
+    // Train set original size: 42000
+    // Val set original size: 18000
+    trainset.resize(8000);
+    valset.resize(1000);
 
+    auto batchSize = 32;
     auto trainLoader = torch::data::make_data_loader(std::move(trainset.map(torch::data::transforms::Stack<>())),
                                                      torch::data::DataLoaderOptions().batch_size(batchSize).drop_last(true));
 
     auto valLoader = torch::data::make_data_loader(std::move(valset.map(torch::data::transforms::Stack<>())),
                                                    torch::data::DataLoaderOptions().batch_size(batchSize).drop_last(true));
-
-    auto trainer = Trainer(std::move(model), &loss, batchSize);
-
-    auto [x, y] = trainer.fit<>(*trainLoader, *valLoader, 10);
-
+    auto trainer = Trainer(model, loss, batchSize, 255.0);
+    auto [x, y] = trainer.fit<>(*trainLoader, *valLoader, 50);
 }
